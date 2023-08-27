a7a2e9ab0b49a688fbcf1b076201fb63d69b8b0c
Sebastian Sierra <133537862+SebastianSierra15@users.noreply.github.com>
2023-08-27 16:32:01 -0500

    Update fraccion.cpp

--- matematicas/fraccion.cpp
+++ matematicas/fraccion.cpp
@@ -2,55 +2,122 @@
 
 using namespace std;
 
-int mcd(int a, int b){
-    return  a? mcd(b %a, a): b;
-}
+typedef long long int ll;
 
-struct fraccion {
-    int num, den;
-
-    fraccion(int x, int y) {
-        num = x; den = y;
-        if (num == 0) den = 1;
-        else {
-            int dividir = mcd(num, den);
-            num /= dividir;
-            den /= dividir;
-        }
-        if (den < 0){ num *= -1;  den *= -1; }
-    }
-
-    fraccion operator+(fraccion b) {//suma
-        return fraccion(num*b.den + b.num*den, den*b.den);
-    }
-    fraccion operator-(fraccion b) {//resta
-        return fraccion(num*b.den - b.num*den, den*b.den);
-    }
-    fraccion operator*(fraccion b) {//multiplicar
-        return fraccion(num*b.num, den*b.den);
-    }
-    fraccion inversa() {
-        return fraccion(den, num);
-    }
-    fraccion operator/(fraccion b) {//dividir
-        return fraccion(num*b.den, b.num*den);
-    }
-    string toString() {
-        stringstream ss;
-        ss << num;
-        if (den == 1) return ss.str();
-        ss << "/"; ss << den;
-        return ss.str();
-    }
+struct fraccion{
+	ll num, den;
+	fraccion(){
+		num = 0, den = 1;
+	}
+	fraccion(ll x, ll y){
+		if(y < 0)
+			x *= -1, y *=-1;
+		ll d = __gcd(abs(x), abs(y));
+		num = x/d, den = y/d;
+	}
+	fraccion(ll v){
+		num = v;
+		den = 1;
+	}
+	fraccion operator+(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return fraccion(num*(f.den/d) + f.num*(den/d), den*(f.den/d));
+	}
+	fraccion operator-() const{
+		return fraccion(-num, den);
+	}
+	fraccion operator-(const fraccion& f) const{
+		return *this + (-f);
+	}
+	fraccion operator*(const fraccion& f) const{
+		return fraccion(num*f.num, den*f.den);
+	}
+	fraccion operator/(const fraccion& f) const{
+		return fraccion(num*f.den, den*f.num);
+	}
+	fraccion operator+=(const fraccion& f){
+		*this = *this + f;
+		return *this;
+	}
+	fraccion operator-=(const fraccion& f){
+		*this = *this - f;
+		return *this;
+	}
+	fraccion operator++(int xd){
+		*this = *this + 1;
+		return *this;
+	}
+	fraccion operator--(int xd){
+		*this = *this - 1;
+		return *this;
+	}
+	fraccion operator*=(const fraccion& f){
+		*this = *this * f;
+		return *this;
+	}
+	fraccion operator/=(const fraccion& f){
+		*this = *this / f;
+		return *this;
+	}
+	bool operator==(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) == (den/d)*f.num);
+	}
+	bool operator!=(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) != (den/d)*f.num);
+	}
+	bool operator >(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) > (den/d)*f.num);
+	}
+	bool operator <(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) < (den/d)*f.num);
+	}
+	bool operator >=(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) >= (den/d)*f.num);
+	}
+	bool operator <=(const fraccion& f) const{
+		ll d = __gcd(den, f.den);
+		return (num*(f.den/d) <= (den/d)*f.num);
+	}
+	fraccion inverso() const{
+		return fraccion(den, num);
+	}
+	fraccion fabs() const{
+		fraccion nueva;
+		nueva.num = abs(num);
+		nueva.den = den;
+		return nueva;
+	}
+	double value() const{
+		return (double)num / (double)den;
+	}
+	string str() const{
+		stringstream ss;
+		ss << num;
+		if(den != 1) ss << "/" << den;
+		return ss.str();
+	}
 };
 
-int main(){
+ostream &operator<<(ostream &os, const fraccion & f) { 
+	return os << f.str();
+}
 
-    int q, w;
-    while(cin >> q >> w){
-        fraccion c(q, w);
-        cout << c.toString() << endl;
-    }
-
-    return 0;
+istream &operator>>(istream &is, fraccion & f){
+	ll num = 0, den = 1;
+	string str;
+	is >> str;
+	size_t pos = str.find("/");
+	if(pos == string::npos){
+		istringstream(str) >> num;
+	}else{
+		istringstream(str.substr(0, pos)) >> num;
+		istringstream(str.substr(pos + 1)) >> den;
+	}
+	f = fraccion(num, den);
+	return is;
 }
